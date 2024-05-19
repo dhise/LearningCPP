688c832f081faf2622112d519b2a20c3f29e4460
Jared Hise <jdhise@gmail.com>
2024-05-18 20:23:11 -0400

    Update testoneswitch.cpp

--- testoneswitch.cpp
+++ testoneswitch.cpp
@@ -1,13 +1,9 @@
-//Follow up on 4.10 Test question
-
-//I edited the switch version and got it working enough to where I'm happy. Not sure how to get it to display the invalid with out having to return a 0. Might revist later.
-//I like this more than branching if statements not sure which one is more efficient or faster
-
 #include <iostream>
 
 
 
-double myCalc(char ch1, double x, double y){
+double myCalc(char ch1, double x, double y)
+{
 
   if (ch1 == '+'|| ch1 == '-' || ch1 == '*' || ch1 == '/' )//checks if user enters one of the options
   {
@@ -23,23 +19,20 @@ double myCalc(char ch1, double x, double y){
           return x / y;
       }
   }
-  else //if anything other than 4 options entered then print invalid
-  {
-    std::cout << "Invalid input. Ending ";
-  }
-  return 0;
+  return 0.00001; //if none of the 4 options are used then return a very unlikely value
+  
 }
 
 
 int main()
   {
 
-    
+
   std::cout << "Enter two fractional numbers\n";
-  
+
   double x {};
   std::cin >> x;
-  
+
 
   double y {};
   std::cin >> y;
@@ -50,12 +43,16 @@ int main()
   char ch1 {};
   std::cin >> ch1;
 
-    
+
   double answer{myCalc(ch1, x, y)};//Takes the char literal and the 2 doubles the user entered and runs it through switch statement
 
-  std::cout << answer;
-  
- 
+  if (answer != 0.00001) //if the value returned isn't the very unlikely one then print the answer
+  {
+    std::cout << x << ' ' << ch1 << ' ' << y << " is " << answer;
+  }
+  else std::cout << "Invalid input. Ending"; //this was a bad way to do this
+
+
 
   return 0;
 }
